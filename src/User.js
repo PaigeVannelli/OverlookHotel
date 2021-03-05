1f7949bd5636c22243c1e6ce7ca1969daf418f0f
Paige Vannelli <paigevannelli@gmail.com>
2021-03-05 14:47:38 -0700

    Create test data files and build user methods

--- src/User.js
+++ src/User.js
@@ -1,6 +1,27 @@
+
+
 class User {
+    constructor(userID, customers, bookings) {
+        this.id = userID;
+        this.customers = customers;
+        this.allBookings = bookings;
+        this.name = this.fetchName();
+        this.userBookings = this.fetchBookings();
+    }
 
+    fetchName() {
+        const currentCustomer = this.customers.find(customer => {
+            return customer.id === this.id
+        })
+        return currentCustomer.name
+    }
 
+    fetchBookings() {
+        const currentCustomerBookings = this.allBookings.filter(booking => {
+            return booking.userID === this.id
+        })
+        return currentCustomerBookings
+    }
 }
 
 export default User
